ca4aca293e432858744548b974c56b97b3eb1c56
Saneef Ansari <hello@saneef.com>
2024-04-21 11:18:03 +0530

    adds content

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,23 @@
 // @ts-check
+import pluginWebC from "@11ty/eleventy-plugin-webc";
+import yaml from "js-yaml";
+import { cssTransforms, htmlTransforms } from "./eleventy/transforms.js";
 
-module.exports = function (eleventyConfig) {
+const INPUT_DIR = "src";
+const OUTPUT_DIR = "dist";
+export default function (eleventyConfig) {
+	eleventyConfig.addDataExtension("yml,yaml", (contents) =>
+		yaml.load(contents)
+	);
 
-};
+	eleventyConfig.addPlugin(pluginWebC, {
+		components: "src/_components/**/*.webc",
+	});
+
+	eleventyConfig.addPlugin(htmlTransforms);
+	eleventyConfig.addPlugin(cssTransforms);
+
+	return {
+		dir: { input: INPUT_DIR, output: OUTPUT_DIR },
+	};
+}
--- prettier.config.js
+++ prettier.config.js
@@ -2,22 +2,25 @@
 
 /** @type {import("prettier").Config} */
 const config = {
-  trailingComma: "es5",
-  useTabs: false,
-  tabWidth: 2,
-  semi: true,
-  singleQuote: false,
-  bracketSpacing: true,
+	trailingComma: "es5",
+	tabWidth: 2,
+	semi: true,
+	singleQuote: false,
+	bracketSpacing: true,
 
-  plugins: ["prettier-plugin-jinja-template"],
-  overrides: [
-    {
-      files: ["*.njk"],
-      options: {
-        parser: "jinja-template",
-      },
-    },
-  ],
+	plugins: ["prettier-plugin-jinja-template"],
+	overrides: [
+		{
+			files: "*.webc",
+			options: { parser: "html" },
+		},
+		{
+			files: ["*.njk"],
+			options: {
+				parser: "jinja-template",
+			},
+		},
+	],
 };
 
 export default config;
